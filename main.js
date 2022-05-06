65926c3932577d39168ffd5a3a25f17809d001eb
NiltonTorres <89040283+NiltonTorres@users.noreply.github.com>
2022-05-06 01:58:32 -0500

    feat: Added routes corrects

--- main.js
+++ main.js
@@ -2,25 +2,31 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import PrimeVue from "primevue/config";
+import Dropdown from "primevue/dropdown";
+import TabView from "primevue/tabview";
+import TabPanel from "primevue/tabpanel";
+import TabMenu from "primevue/tabmenu";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import ColumnGroup from "primevue/columngroup"; //optional for column grouping
+import Row from "primevue/row";
+import Button from "primevue/button";
+import Dialog from "primevue/dialog";
+import Calendar from "primevue/calendar";
+import InputText from "primevue/inputtext";
 
 //PrimeVue Styles
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 import "primevue/resources/themes/md-light-indigo/theme.css";
 import "primevue/resources/themes/mdc-dark-indigo/theme.css";
-import DataTable from "primevue/datatable";
-import Column from "primevue/column";
-import Button from "primevue/button";
+import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
+import "/node_modules/primeflex/primeflex.css";
 import Toolbar from "primevue/toolbar";
-import InputText from "primevue/inputtext";
 import Sidebar from "primevue/sidebar";
-import Row from "primevue/row";
-import Dialog from "primevue/dialog";
 import Toast from "primevue/toast";
-import Dropdown from "primevue/dropdown";
 import Tag from "primevue/tag";
 import Card from "primevue/card";
-import TabView from "primevue/tabview";
 
 const app = createApp(App);
 
@@ -42,7 +48,10 @@ app.component("pv-toast", Toast);
 app.component("pv-dropdown", Dropdown);
 app.component("pv-tag", Tag);
 app.component("pv-card", Card);
+app.component("pv-calendar", Calendar);
 app.component("pv-tabview", TabView);
+app.component("pv-column-group", ColumnGroup);
+app.component("pv-tab-panel", TabPanel);
+app.component("pv-tab-menu", TabMenu);
+
 app.mount("#app");
-
-
