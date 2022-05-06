38bae6c14f460c10e13a601b3ea6201d1ca0637e
Italo Silva <italo.silva.silvera@outlook.com>
2022-05-06 06:54:10 -0500

    chore: develop change status

--- src/Shipments/pages/shipments-list.component.vue
+++ src/Shipments/pages/shipments-list.component.vue
@@ -4,11 +4,20 @@
       <pv-column v-for="col in columns" :field="col.field" :header="col.header" :key="col.field"></pv-column>
       <pv-column :exportable="false" style="min-width: 8rem">
         <template #body="slotProps">
-          <pv-button v-if="enableListDialogs" icon="pi pi-car" class="p-button-text p-button-rounded"/>
+          <pv-button v-if="enableListDialogs" icon="pi pi-car" class="p-button-text p-button-rounded" @click="showStatus"/>
           <pv-button icon="pi pi-eye" class="p-button-text p-button-rounded" @click="showDialog"/>
         </template>
       </pv-column>
     </pv-data-table>
+    <pv-dialog v-model:visible="statusEnabled">
+      <template #header>
+        <h3>Change actual state</h3>
+      </template>
+      <shipments-status></shipments-status>
+      <template #footer>
+        <pv-button label="Submit" autofocus @click="showStatus"/>
+      </template>
+    </pv-dialog>
     <pv-dialog v-model:visible="dialogEnabled">
       <template #header>
         <h3>Current Location</h3>
@@ -24,11 +33,16 @@
 
 <script>
 import { ShipmentsApiService } from "../service/Shipments-api.service"
+import ShipmentsStatus from "./shipments-status.component.vue";
 
 export default {
   name: "shipments-list",
+  components: {ShipmentsStatus},
   data(){
     return {
+      components: {
+        ShipmentsStatus
+      },
       shipmentsService: null,
       shipments: [],
       columns: [
@@ -40,7 +54,8 @@ export default {
         {field: 'status', header: 'Status'}
       ],
       currentShipments: [],
-      dialogEnabled: false
+      dialogEnabled: false,
+      statusEnabled: false
     }
   },
   created(){
@@ -50,6 +65,7 @@ export default {
       this.currentShipments = this.shipments;
     });
   },
+
   props: {
     enableListDialogs: Boolean
   },
@@ -74,7 +90,10 @@ export default {
     },
     showDialog(){
       this.dialogEnabled = !this.dialogEnabled;
-    }
+    },
+    showStatus(){
+      this.statusEnabled = !this.statusEnabled;
+    },
   }
 }
 </script>
--- src/Shipments/service/Shipments-api.service.js
+++ src/Shipments/service/Shipments-api.service.js
@@ -1,10 +1,14 @@
 import http from "../../core/services/http-common";
 
 export class ShipmentsApiService {
+    endPoint = 'http://localhost:3001/shipments';
     getAll(){
         return http.get("/shipments");
     }
     create(data){
         return http.post("/shipments",data);
+    }
+    update(id, updateShipmentStatus) {
+        return http.put(`${this.endPoint}/${id}`, updateShipmentStatus);
     }
 }
