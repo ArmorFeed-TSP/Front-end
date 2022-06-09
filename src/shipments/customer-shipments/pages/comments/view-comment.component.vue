283fcd4d1a0a1ed7cd474dc06e96bc3936e2a025
Nilton Torres <u201924058@upc.edu.pe>
2022-06-08 23:55:38 -0500

    feat: Added component view comment

---
+++ src/shipments/customer-shipments/pages/comments/view-comment.component.vue
@@ -0,0 +1,86 @@
+<template>
+  <pv-scroll-panel class="scroll-comment" style="width: 27rem; height: 170px">
+    <div v-if="displayBoxComment">
+      <pv-card
+        v-for="(comment, index) in comments"
+        v-bind:key="index"
+        class="card-comment"
+      >
+        <template #title>
+          {{ comment.title }}
+        </template>
+        <template #content>
+          {{ comment.content }}
+        </template>
+      </pv-card>
+    </div>
+    <div v-else>
+      <pv-card>
+        <template #title> has no comments </template>
+      </pv-card>
+    </div>
+  </pv-scroll-panel>
+</template>
+
+<script>
+import { CommentsApiService } from "../../services/comments-api.service";
+
+
+export default {
+  name: "view-comment",
+  props: {
+
+  },
+  data() {
+    return {
+      comments: [],
+      commentDialog: false,
+      deleteCommentDialog: false,
+      deleteCommentsDialog: false,
+      comment: {},
+      filters: {},
+      submitted: false,
+      commentsService: null,
+      selectedComments: null,
+      sizeComments: 0,
+      displayBoxComment: true,
+      userId: null,
+    };
+  },
+  created() {
+    const auth=JSON.parse(localStorage.getItem("auth"));
+    if(auth) {
+      this.userId = auth.user.id
+    }
+    this.commentsService = new CommentsApiService();
+    this.commentsService
+      .getCommentsByEnterpriseId(this.userId)
+      .then((response) => {
+        this.comments = response.data;
+        this.comments.forEach((comment) => this.getDisplayableComment(comment));
+        this.sizeComments = this.comments.length;
+        console.log(this.sizeComments, "size");
+        if (this.sizeComments === 0) {
+          this.displayBoxComment = false;
+        }
+      });
+  },
+  methods: {
+    getDisplayableComment(comment) {
+      comment.id = this.comment.id;
+      return comment;
+    },
+    getStorableComment(displayableComment) {
+      return {
+        id: displayableComment.id,
+        title: displayableComment.title,
+        content: displayableComment.content,
+        enterpriseId: displayableComment.enterpriseId,
+        customerId: displayableComment.customerId,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped></style>
