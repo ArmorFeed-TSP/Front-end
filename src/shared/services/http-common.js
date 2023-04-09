a11d6fbdef45bf4fe2926f244f9a80c64ef5bde4
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2023-04-08 19:43:30 -0500

    feat: added new web api url

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5017/api/v1",
+  baseURL: "https://back-end-production-fb3b.up.railway.app/api/v1",
   headers: { "Content-type": "application/json" },
 });
