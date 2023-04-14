4a8c7185d07b1439e39777ace3a86ed28d8ffcb5
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2023-04-13 23:00:38 -0500

    hotfix: branch ready to deploy

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://back-end-production-fb3b.up.railway.app/api/v1",
+  baseURL: "https://back-end-production-9e82.up.railway.app/api/v1",
   headers: { "Content-type": "application/json" },
 });
