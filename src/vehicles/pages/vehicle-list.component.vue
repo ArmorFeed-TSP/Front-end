e3624cfae1ba75b1a401bd3d05f10fc571644ad8
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2023-05-04 01:27:12 -0500

    feat: added vehicle image form field in enterprise vehicles tab

--- src/vehicles/pages/vehicle-list.component.vue
+++ src/vehicles/pages/vehicle-list.component.vue
@@ -1,4 +1,5 @@
 <template>
+  <pv-toast/>
   <div class="container">
     <div class="flex flex-column space-between align-items-center">
       <div class="my-7 flex flex-column" style="max-width: 65rem; width: 100%; margin: 0 auto; min-height: 90vh">
@@ -48,6 +49,20 @@
             :sortable="true"
             style="min-width: 16rem"
           ></pv-column>
+          <pv-column
+            field="image"
+            header="Image"
+            :sortable="false"
+            style="min-width: 16rem"
+          >
+            <template #body="slotProps">
+              <img 
+              :src="`${slotProps.data.image}`" 
+              alt="Vehicle Image"
+              class="w-6rem shadow-2 border-round"
+              />
+            </template>
+          </pv-column>
           <pv-column
             field="licensePlate"
             header="License Plate"
@@ -171,6 +186,18 @@
               </span>
             </div>
             <div class="field">
+              <div class="card flex justify-content-center">
+                <pv-file-upload
+                  mode="basic"
+                  name="demo[]"
+                  url="/api/upload"
+                  accept="image/*"
+                  customUpload
+                  @uploader="uploadImage"
+                />
+              </div>
+            </div>
+            <div class="field">
               <span class="p-float-label">
                 <pv-calendar
 
@@ -285,6 +312,7 @@
 <script>
 import { FilterMatchMode } from "primevue/api";
 import { VehiclesApiService } from "../services/vehicle-api.service";
+import { Base64Manager } from "../../shared/services/base64-uploader.service";
 export default {
   name: "vehicle-list",
   components: {},
@@ -308,6 +336,8 @@ export default {
         { type: "In use", code: "In use" },
         { type: "Free", code: "Free" },
       ],
+      imageUploader: new Base64Manager(),
+      imageDataHandler: { data: null }
     };
   },
 
@@ -324,8 +354,10 @@ export default {
         this.vehicles = response.data;
         this.vehicles.forEach((vehicle) => this.getDisplayableVehicle(vehicle));
         console.log("created");
+        console.log(this.vehicles);
       });
     this.initFilters();
+    this.imageUploader = new Base64Manager();
   },
   methods: {
     getDisplayableVehicle(vehicle) {
@@ -340,7 +372,12 @@ export default {
         year: displayableVehicle.year,
         model: displayableVehicle.model,
         maintenanceDate: displayableVehicle.maintenanceDate,
+<<<<<<< Updated upstream
         vehicleType: displayableVehicle.vehicleType,
+=======
+        image: this.imageDataHandler.data,
+        currentState: displayableVehicle.currentState,
+>>>>>>> Stashed changes
         enterpriseId: displayableVehicle.enterpriseId,
       };
     },
@@ -357,6 +394,15 @@ export default {
       return this.vehicles.findIndex((vehicle) => vehicle.id === id);
     },
     saveVehicle() {
+      if(this.imageDataHandler.data === null) {
+        this.$toast.add({
+          severity: "error",
+          summary: "Is missing some field",
+          detail: "You must upload a image",
+          life: 3000,
+        });
+        return;
+      }
       this.submitted = true;
       if (this.vehicle.brand.trim()) {
         if (this.vehicle.id) {
@@ -373,12 +419,13 @@ export default {
                 life: 3000,
               });
               console.log(response);
+              this.imageDataHandler = { data: null};
             });
         } else {
           this.vehicle.id = 0;
           this.vehicle.enterpriseId = this.userId;
-          console.log(this.vehicle);
           this.vehicle = this.getStorableVehicle(this.vehicle);
+          console.log(this.vehicle);
           this.vehiclesService.create(this.vehicle).then((response) => {
             this.vehicle = this.getDisplayableVehicle(response.data);
             this.vehicles.push(this.vehicle);
@@ -389,6 +436,7 @@ export default {
               life: 3000,
             });
             console.log(response);
+            this.imageDataHandler = { data: null};
           });
         }
         this.vehicleDialog = false;
@@ -434,6 +482,9 @@ export default {
         global: { value: null, matchMode: FilterMatchMode.CONTAINS },
       };
     },
+    async uploadImage(event) {
+     await this.imageUploader.upload(event, this.imageDataHandler);
+    }
   },
 };
 </script>
