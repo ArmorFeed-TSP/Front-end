0b51d8cebe358c7c147efd0ccf21a40d7938964f
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2023-05-05 01:26:35 -0500

    fix: added backend deployed url

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5017/api/v1",
+  baseURL: "https://webapp-armorfeed-54267432.azurewebsites.net/api/v1",
   headers: { "Content-type": "application/json" },
 });
