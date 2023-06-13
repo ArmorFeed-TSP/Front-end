97ed4d773ff9d3d09bd2d55a1ae6a6338a299c6d
Jesús Pacheco <jesuspacheco0219@gmail.com>
2023-06-12 20:03:44 -0500

    add new state for vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,14 @@
 import { createStore } from "vuex";
 import { auth } from "./auth.module";
 const store = createStore({
+  state: {
+    darkMode: false,
+  },
+  mutations: {
+    setDarkMode(state, isDark) {
+      state.darkMode = isDark;
+    },
+  },
   modules: {
     auth,
   },
