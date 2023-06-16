840ccd2e3d892925db03e7b1fb021e74d73ce8ec
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2023-06-16 11:00:36 -0500

    feature: added base url of deployed api

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Base Api url
 export default axios.create({
-  //baseURL: "https://armorfeedapitsp.azurewebsites.net/api/v1",
-  baseURL: "http://localhost:5017/api/v1",
+  baseURL: "https://armorfeedapitsp.azurewebsites.net/api/v1",
+  //baseURL: "http://localhost:5017/api/v1",
   headers: { "Content-type": "application/json" },
 });
